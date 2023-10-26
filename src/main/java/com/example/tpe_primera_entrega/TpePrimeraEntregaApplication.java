edb959d0921caffebce5263be50abf29f58a6135
Santiago Lázaro <santi.lazaro@gmail.com>
2023-10-26 15:06:58 -0300

    first push

---
+++ src/main/java/com/example/tpe_primera_entrega/TpePrimeraEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpe_primera_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpePrimeraEntregaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpePrimeraEntregaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpe_primera_entrega/TpePrimeraEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpe_primera_entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpePrimeraEntregaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
